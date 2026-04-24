7cdaa70b887c29d7159ade94e932e3eee208535c
Abhista-111005 <abhistachakraborty15@gmail.com>
2026-04-25 00:10:03 +0530

    completed Store Api , store service , implementation of the store service and updated data in store repository

--- SaaS-POS-System/src/main/java/com/abhi/repository/StoreRepository.java
+++ SaaS-POS-System/src/main/java/com/abhi/repository/StoreRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store , Long> {
 
-    Store findByStoreAdminId(Long adminId);
+    Store findByStoreAdminId(String Id);
 
 }
